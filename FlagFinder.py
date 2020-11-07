9ff93d63ae54b99dac7a4582b7f0ce33392252a1
tiphergane <tiphergane@gmail.com>
2020-11-07 21:37:18 +0100

    modifié :         FlagFinder.py

--- FlagFinder.py
+++ FlagFinder.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#coding: utf-8
+# coding: utf-8
 from pwn import *
 import sys
 import re
@@ -7,23 +7,26 @@ import re
 regex = sys.argv[2]
 file = sys.argv[1]
 
+
 def Exploit():
     info("Opening file: %s\n" % file)
     f = read(file)
     info("Searching for pattern: %s\n" % regex)
-    c = re.findall(regex,str(f))
+    c = re.findall(regex, str(f))
     if not str(c):
         info("Too Bad my friend !!!")
     else:
         for a in c:
             success("Yeah !!!! flag found: %s\n" % a)
             success("flag is now copied in flag.txt")
-            f = open("flag.txt","w")
+            f = open("flag.txt", "w")
             f.write(a)
             f.close()
 
+
 def failExploit():
     warn("Failed to open file %s" % file)
+
 
 try:
     Exploit()
