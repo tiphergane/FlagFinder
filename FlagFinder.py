3253f1e2e10bc672c2fcc3eb0cafdb69792935b0
tiphergane <tiphergane@gmail.com>
2021-05-04 15:48:21 +0200

    "Mise à jour de sys.args vers argparse, merci le challenge FCSC"

--- FlagFinder.py
+++ FlagFinder.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 # coding: utf-8
+
 import pwn
-import sys
+import argparse
 import re
 
 
-prog = sys.argv[0]
-
-
-def Exploit():
-    regex = sys.argv[2]
-    file = sys.argv[1]
+def Exploit(fichier, pattern):
+    regex = pattern
+    file = fichier
     pwn.info("Opening file: {fichier}\n".format(fichier=file))
     s = pwn.read(file)
     pwn.info("Searching for pattern: {flag}\n".format(flag=regex))
@@ -28,22 +26,29 @@ def failExploit():
     pwn.error("An error occured")
 
 
-if len(sys.argv) != 3:
-    pwn.warn(
-        "Usage: {program} {file} {regex}".format(
-            program=sys.argv[0], file="cible", regex="flag{.*?}"
-        )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Rechercher un flag dans un fichier", prog="FlagFinder.py"
     )
-    sys.exit()
-
-try:
-    Exploit()
-except IOError:
-    pwn.error("Impossible d'ouvrir le fichier {file}".format(file=sys.argv[1]))
-except KeyboardInterrupt:
-    pwn.info("Canceled by user (CTRL-C)")
-except:
-    failExploit()
-finally:
-    pwn.info("Goodbye Professor !")
-    sys.exit()
+    parser.add_argument(
+        "-f",
+        "--file",
+        dest="fichier",
+        help="Fichier à vérifier",
+    )
+    parser.add_argument(
+        "-r",
+        "--regex",
+        dest="pattern",
+        help="Pattern a chercher",
+        default="FLAG{.*?}",
+    )
+    args = parser.parse_args()
+    fichier = args.fichier
+    pattern = args.pattern
+    try:
+        Exploit(fichier, pattern)
+    except:
+        failExploit()
+    finally:
+        pwn.info("Goodbye Professor !")
